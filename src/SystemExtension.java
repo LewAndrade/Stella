b71313e345a01533a72ab95c546bcfef2f83c639
Lucas Andrade <andrade.lucasap@gmail.com>
2020-05-13 18:17:25 -0300

    Created SystemExtension class

---
+++ src/SystemExtension.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SystemExtension {
+    public static void print(Object arg) {
+        System.out.println(arg);
+    }
+
+    public static void print() {
+        System.out.println();
+    }
+
+    public static void printSl(Object arg) {
+        System.out.print(arg);
+    }
+
+    public static void stop(){
+        System.exit(0);
+    }
+
+    public static boolean isFileEmpty(File file) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(file.getPath()));
+        return br.readLine() == null && file.length() == 0;
+    }
+
+    public static boolean isInteger(String s, int radix) {
+        Scanner sc = new Scanner(s.trim());
+        if(!sc.hasNextInt(radix)) return false;
+        // we know it starts with a valid int, now make sure
+        // there's nothing left!
+        sc.nextInt(radix);
+        return !sc.hasNext();
+    }
+
+}
