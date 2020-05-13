8f88869823e7fa5f71b54a1633d2510853ac305b
Lucas Andrade <andrade.lucasap@gmail.com>
2020-05-13 18:12:44 -0300

    Created User class

---
+++ src/User.java
@@ -0,0 +1,38 @@
+public class User {
+    String name;
+    int age;
+    int state = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", state=" + state +
+                '}';
+    }
+}
