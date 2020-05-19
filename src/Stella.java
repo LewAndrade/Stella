d7032e2674ef5f6e4d3019c5905426aa97bf1c5f
Lucas Andrade <andrade.lucasap@gmail.com>
2020-05-19 11:59:10 -0300

    Created mainMenu structure and ADD device method to it

--- src/Stella.java
+++ src/Stella.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Stella {
 
@@ -6,17 +7,44 @@ public class Stella {
         User user;
 
         if (UserExtension.userData.exists()) {
-            if (SystemExtension.isFileEmpty(UserExtension.userData)) {
+            if (StellaExtension.isFileEmpty(UserExtension.userData)) {
                 user = UserExtension.createNewUser();
             } else {
                 user = UserExtension.readUserFromFile();
             }
+            mainMenu(user);
         } else {
             if (UserExtension.createUserDataFile()) {
                 user = UserExtension.createNewUser();
+                mainMenu(user);
             } else {
-                SystemExtension.stop();
+                StellaExtension.stop();
             }
         }
+    }
+
+    public static void mainMenu(User user) throws IOException {
+        StellaExtension.Ui.greetings(user.getName());
+        var choice = StellaExtension.Ui.optionsMenu();
+        while (choice != Options.EXIT) {
+            switch (choice) {
+                case ADD:
+                    user.addDevice(DeviceExtension.createNewDevice());
+                    break;
+                case DELETE:
+                    break;
+                case CHECK:
+                    break;
+                case GUIDE:
+                    break;
+                case DETAILS:
+                    break;
+                case EDIT:
+                    break;
+            }
+            choice = StellaExtension.Ui.optionsMenu();
+        }
+        StellaExtension.Ui.farewell();
+        UserExtension.writeUserInFile(user);
     }
 }
