37c7ca6b0fba028a0150e4d77ad98c18cd708427
Lewmcas <Lewmcas@gmail.com>
2020-05-25 18:00:12 -0300

    Refactored Main Stella and Created User Methods:
    Energy Consumption / Cost calculation
    Consumption Guidance
    CRUD methods for user data
    CRUD methods for properties

--- src/Stella.java
+++ src/Stella.java
@@ -1,55 +1,41 @@
-import java.io.IOException;
-
 public class Stella {
 
-    public static void main(String[] args) throws IOException {
-        User user;
-
-        if (UserExtension.userData.exists()) {
-            if (StellaExtension.isFileEmpty(UserExtension.userData)) {
-                user = UserExtension.createNewUser();
-            } else {
-                user = UserExtension.readUserFromFile();
-            }
-        } else {
-            UserExtension.createUserDataFile();
-            user = UserExtension.createNewUser();
-        }
-        mainMenu(user);
+    public static void main(String[] args) {
+        User user = initializeUser();
+        if (user != null) mainMenu(user);
     }
 
-    public static void mainMenu(User user) throws IOException {
+    private static User initializeUser() {
+        if (UserExtension.createUserFile() || UserExtension.isUserFileEmpty()) {
+            return UserExtension.createUserData();
+        } else {
+            return UserExtension.loadUserData();
+        }
+    }
+
+    private static void mainMenu(User user) {
+        StellaExtension.Ui.logo();
         StellaExtension.Ui.greetings(user.getName());
-        var choice = StellaExtension.Ui.optionsMenu();
+        Option choice = StellaExtension.Ui.optionsMenu();
         while (choice != Option.EXIT) {
             switch (choice) {
                 case ADD_DEVICE:
-                    user.addDevice(DeviceExtension.createNewDevice());
-                    UserExtension.writeUserInFile(user);
+                    DeviceExtension.createNewDevice(user);
                     break;
                 case EDIT_DEVICE:
-                    var repeatEdit = true;
-                    while (repeatEdit) {
-                        DeviceExtension.listDevices(user.getDevices());
-                        var editedDevice = DeviceExtension.editDevice(user);
-                        UserExtension.writeUserInFile(user);
-                        repeatEdit = editedDevice && StellaExtension.Ui.setEditRepeat();
-                    }
+                    DeviceExtension.editDevice(user);
                     break;
                 case DELETE_DEVICE:
-                    var repeatDelete = true;
-                    while(repeatDelete) {
-                        DeviceExtension.listDevices(user.getDevices());
-                        var deletedDevice = DeviceExtension.deleteDevice(user);
-                        UserExtension.writeUserInFile(user);
-                        repeatDelete = deletedDevice && StellaExtension.Ui.setDeleteRepeat();
-                    }
-                    break;
-                case CONSUMPTION_GUIDE:
+                    DeviceExtension.deleteDevice(user);
                     break;
                 case CHECK_CONSUMPTION:
+                    UserExtension.getEnergyConsumption(user);
+                    break;
+                case CONSUMPTION_GUIDE:
+                    UserExtension.getConsumptionGuide(user);
                     break;
                 case EDIT_PROFILE:
+                    UserExtension.editUserData(user);
                     break;
             }
             choice = StellaExtension.Ui.optionsMenu();
