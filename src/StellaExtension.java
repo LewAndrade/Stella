57e1fa6bac170e3fdbd03dc6113a7668d04dee17
Lewmcas <Lewmcas@gmail.com>
2020-05-25 18:15:50 -0300

    Refactored StellaExtension architecture

--- src/StellaExtension.java
+++ src/StellaExtension.java
@@ -1,24 +1,9 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class StellaExtension {
-    public static List<String> continueList = Arrays.asList("sim", "si", "s", "yes", "ye", "y", "zim", "yup", "ja",
-            "uhum", "ss", "yip", "yabadabadoo", "okiedokie", "yeet", "okay", "okie", "ok");
-
-    public static void stop() {
-        System.exit(0);
-    }
-
-    public static boolean isFileEmpty(File file) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(file.getPath()));
-        return br.readLine() == null && file.length() == 0;
-    }
 
     public static boolean isInteger(String string) {
         Scanner sc = new Scanner(string.trim());
@@ -27,7 +12,7 @@ public class StellaExtension {
         return !sc.hasNext();
     }
 
-    private static boolean isNumeric(String string) {
+    public static boolean isNumeric(String string) {
         try {
             Double.parseDouble(string);
             return true;
@@ -37,16 +22,20 @@ public class StellaExtension {
     }
 
     public static boolean isValidNameFormat(String string) {
-        return string.length() <= 32 && (!string.isBlank() || !string.isEmpty());
+        return string.length() <= 32 && string.length() > 0;
     }
 
-    public static boolean isValidPowerFormat(String string) {
-        return isNumeric(string);
+    public static boolean isPositiveDouble(String string) {
+        if (isNumeric(string)) {
+            double value = Double.parseDouble(string);
+            return value > 0;
+        }
+        return false;
     }
 
     public static boolean isValidHourFormat(String string) {
         if (isNumeric(string)) {
-            var value = Double.parseDouble(string);
+            double value = Double.parseDouble(string);
             return !(value > 24) && !(value < 0);
         }
         return false;
@@ -55,7 +44,7 @@ public class StellaExtension {
     public static boolean isValidAgeFormat(String string) {
         if (isNumeric(string)) {
             if (isInteger(string)) {
-                var value = Double.parseDouble(string);
+                double value = Double.parseDouble(string);
                 return !(value > 120) && !(value < 0);
             } else {
                 return false;
@@ -65,8 +54,8 @@ public class StellaExtension {
     }
 
     public static TimeFormat getTimeFormat(String string) {
-        var hourFormats = Arrays.asList("horas", "hora", "h", "hr", "hrs", "em horas");
-        var minuteFormats = Arrays.asList("minutos", "minuto", "min", "m", "em minutos");
+        List<String> hourFormats = Arrays.asList("horas", "hora", "h", "hr", "hrs", "em horas");
+        List<String> minuteFormats = Arrays.asList("minutos", "minuto", "min", "m", "em minutos");
         if (hourFormats.contains(string)) return TimeFormat.HOUR;
         else if (minuteFormats.contains(string)) return TimeFormat.MINUTE;
         else return TimeFormat.NULL;
@@ -81,102 +70,146 @@ public class StellaExtension {
     }
 
     public static class Ui {
+        private static final List<String> continueList = Arrays.asList("sim", "si", "s", "yes", "ye", "y", "zim", "yup", "ja",
+                "uhum", "ss", "yip", "yabadabadoo", "okiedokie", "yeet", "okay", "okie", "ok");
+
         private static final Scanner scanner = new Scanner(System.in);
 
-        public static void headerLine() {
-            Stream.generate(() -> "*").limit(138).forEach(System.out::print);
-            System.out.println();
+        public static void logo() {
+            System.out.println(
+                    "_____/\\\\\\\\\\\\\\\\\\\\\\_________________________________/\\\\\\\\\\\\_____/\\\\\\\\\\\\___________________        \n" +
+                    " ___/\\\\\\/////////\\\\\\______________________________\\////\\\\\\____\\////\\\\\\___________________       \n" +
+                    "  __\\//\\\\\\______\\///______/\\\\\\________________________\\/\\\\\\_______\\/\\\\\\___________________      \n" +
+                    "   ___\\////\\\\\\__________/\\\\\\\\\\\\\\\\\\\\\\_____/\\\\\\\\\\\\\\\\_____\\/\\\\\\_______\\/\\\\\\_____/\\\\\\\\\\\\\\\\\\____     \n" +
+                    "    ______\\////\\\\\\______\\////\\\\\\////____/\\\\\\/////\\\\\\____\\/\\\\\\_______\\/\\\\\\____\\////////\\\\\\___    \n" +
+                    "     _________\\////\\\\\\______\\/\\\\\\_______/\\\\\\\\\\\\\\\\\\\\\\_____\\/\\\\\\_______\\/\\\\\\______/\\\\\\\\\\\\\\\\\\\\__   \n" +
+                    "      __/\\\\\\______\\//\\\\\\_____\\/\\\\\\_/\\\\__\\//\\\\///////______\\/\\\\\\_______\\/\\\\\\_____/\\\\\\/////\\\\\\__  \n" +
+                    "       _\\///\\\\\\\\\\\\\\\\\\\\\\/______\\//\\\\\\\\\\____\\//\\\\\\\\\\\\\\\\\\\\__/\\\\\\\\\\\\\\\\\\__/\\\\\\\\\\\\\\\\\\_\\//\\\\\\\\\\\\\\\\/\\\\_ \n" +
+                    "        ___\\///////////_________\\/////______\\//////////__\\/////////__\\/////////___\\////////\\//__"
+            );
         }
 
         public static void greetings(String username) {
-            StellaExtension.Ui.headerLine();
-            System.out.println("Oi " + username + "!!!");
+            System.out.println("\nOi " + username + "!!!");
 
         }
 
         public static Option optionsMenu() {
-            StellaExtension.Ui.headerLine();
-            System.out.println("--- O que deseja fazer? \n" +
-                    "[1] Adicionar um aparelho\n" +
-                    "[2] Editar um aparelho\n" +
-                    "[3] Deletar um aparelho\n" +
-                    "[4] Descobrir como gastar menos\n" +
-                    "[5] Consultar consumo de energia\n" +
-                    "[6] Alterar dados pessoais\n" +
-                    "[0] Sair");
-            StellaExtension.Ui.headerLine();
+            System.out.print("\n--- O que você vai fazer? \n" +
+                    "╔═════════════════════════════════╗\n" +
+                    "║[1] Adicionar um aparelho        ║\n" +
+                    "║[2] Editar um aparelho           ║\n" +
+                    "║[3] Deletar um aparelho          ║\n" +
+                    "║[4] Consultar consumo de energia ║\n" +
+                    "║[5] Descobrir como gastar menos  ║\n" +
+                    "║[6] Alterar dados pessoais       ║\n" +
+                    "║[0] Sair                         ║\n" +
+                    "╚═════════════════════════════════╝\n");
             System.out.print("Sua escolha: ");
-            var choiceBuffer = scanner.nextLine().strip().toLowerCase();
+            String choiceBuffer = scanner.nextLine().trim().toLowerCase();
 
             do {
                 if (isInteger(choiceBuffer)) {
                     int choice = Integer.parseInt(choiceBuffer);
                     if (Option.indexValues().contains(Integer.parseInt(choiceBuffer))) {
-                        StellaExtension.Ui.headerLine();
                         return Option.fromInt(choice);
                     }
                 }
-                System.out.print("Desculpa, mas acho que você não colocou uma das opções acima, tenta de novo? \n" +
+                System.out.print("\nDesculpa, mas acho que você não colocou uma das opções acima, tenta de novo? \n" +
                         "Sua escolha: ");
-                choiceBuffer = scanner.nextLine().strip().toLowerCase();
+                choiceBuffer = scanner.nextLine().trim().toLowerCase();
 
             } while (true);
         }
 
+        public static int selectOption(int numberOfOptions) {
+            System.out.print("Sua escolha: ");
+            String choiceBuffer = scanner.nextLine().trim();
+            do {
+                if (StellaExtension.isInteger(choiceBuffer)) {
+                    int choice = Integer.parseInt(choiceBuffer);
+                    if (choice >= 0 && choice <= numberOfOptions)
+                        return choice;
+                }
+                StellaExtension.Ui.Error.invalidNumberFormatError();
+                choiceBuffer = scanner.nextLine().trim();
+            } while (true);
+        }
+
         public static void farewell() {
-            System.out.println("Saindo.....\n" +
+            System.out.println("\nPoxa, mas ja vai? :(\n" +
+                    "...tudo bem então...\n" +
+                    "Saindo.....\n" +
                     "Até mais :)");
         }
 
-        public static boolean setEditRepeat() {
-            System.out.print("Deseja editar outra coisa?\n" +
+        public static boolean getConsent() {
+            System.out.print("\nDeseja mesmo fazer isso?\n" +
                     "[sim | nao]: ");
-            return continueList.contains(scanner.nextLine().strip().toLowerCase());
-        }
-
-        public static boolean setDeleteRepeat() {
-            System.out.print("Deseja deletar outra coisa?\n" +
-                    "[sim | nao]: ");
-            return continueList.contains(scanner.nextLine().strip().toLowerCase());
+            return continueList.contains(scanner.nextLine().trim().toLowerCase());
         }
 
         public static class Error {
             public static void invalidHourFormatError() {
-                System.out.print("Desculpa, mas parece que você não escreveu um número válido, tenta de novo vai... \n" +
+                System.out.print("\nDesculpa, mas parece que você não escreveu um número válido, tenta de novo vai... \n" +
                         "Lembra que só tem 24 horas em um dia, não pode passar disso, e nem ser menor que 0 ok? \n" +
                         "--- Horas em uso (h) --- : ");
             }
 
             public static void invalidMinuteFormatError() {
-                System.out.print("Desculpa, mas parece que você não escreveu um número válido, tenta de novo vai... \n" +
+                System.out.print("\nDesculpa, mas parece que você não escreveu um número válido, tenta de novo vai... \n" +
                         "Lembra que só tem 1440 minutos em um dia, não pode passar disso, e nem ser menor que 0 ok? \n" +
                         "--- Minutos em uso (h) --- : ");
             }
 
             public static void invalidNumberFormatError() {
-                System.out.print("Desculpa, mas parece que você não escreveu um número válido, tenta de novo vai...\n" +
+                System.out.print("\nPuts, parece que você não escreveu um número válido, tenta de novo vai...\n" +
                         "Sua escolha: ");
             }
 
             public static void invalidNameFormatError() {
-                System.out.print("Opa, eu acho que você não colocou um nome válido \n" +
+                System.out.print("\nOpa, eu acho que você não colocou um nome válido. \n" +
                         "Tenta de novo por favor? \n" +
                         "--- Nome --- : ");
             }
 
             public static void invalidPowerFormatError() {
-                System.out.print("Desculpa, mas você não escreveu um numero válido pra potencia... \n" +
+                System.out.print("\nDesculpa, mas você não escreveu um numero válido pra potencia... \n" +
                         "--- Potência (W) --- : ");
             }
 
             public static void invalidTimeFormatError() {
-                System.out.print("Desculpa não entendi, você quis dizer horas ou minutos? : ");
+                System.out.print("\nPera não entendi, você quis dizer horas ou minutos? : ");
             }
 
             public static void invalidAgeFormatError() {
-                System.out.print("Por favor coloque um número valido para idade: ");
+                System.out.print("\nEi, esa não é uma idade valida: ");
 
             }
+
+            public static void emptyDeviceList() {
+                System.out.println("\nPoxa, você não tem nenhum aparelho cadastrado,\n" +
+                        "volta aqui depois que você tiver adicionado pelo menos um.");
+            }
+
+            public static void emptyOrZeroedList() {
+                System.out.println("\nEi me diz, você não tem nenhum aparelho ou seus aparelhos não gastam nada?\n" +
+                        "Volta aqui depois que você tiver adicionado pelo menos um..");
+            }
+
+            public static void unableToCreateUserFile() {
+                System.out.println("\n AIAIAI,aconteceu um ERRO :( Eu não consegui salvar os dados de usuario, \n" +
+                        "se isso acontecer de novo, tenta me reiniciar como administrador.");
+            }
+
+            public static void unableToReadUserFile() {
+                System.out.println("\n AIAIAI, aconteceu um ERRO :( Eu não consegui ler os dados de usuario, \n" +
+                        "se isso acontecer de novo, tenta me reiniciar como administrador.");
+            }
+        }
+
+        public static String getDoubleString(double value) {
+            return new DecimalFormat("#.##").format(value);
         }
     }
 }
@@ -185,12 +218,12 @@ enum Option {
     ADD_DEVICE(1),
     EDIT_DEVICE(2),
     DELETE_DEVICE(3),
-    CONSUMPTION_GUIDE(4),
-    CHECK_CONSUMPTION(5),
+    CHECK_CONSUMPTION(4),
+    CONSUMPTION_GUIDE(5),
     EDIT_PROFILE(6),
     EXIT(0);
 
-    int index;
+    final int index;
 
     Option(int index) {
         this.index = index;
